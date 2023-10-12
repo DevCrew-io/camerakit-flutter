bbb7ee1463f2f262fe6bcd97310d4c280744a471
Muhammad Arslan Khan <arslan.khan@devcrew.io>
2023-10-12 13:36:50 +0500

    feat: show media result widget

---
+++ example/lib/media_result_screen.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class MediaResultWidget extends StatefulWidget {
+  final String filePath;
+  final String fileType;
+
+  const MediaResultWidget(
+      {super.key, required this.filePath, required this.fileType});
+
+  @override
+  State<MediaResultWidget> createState() => _CameraResultWidgetState();
+}
+
+class _CameraResultWidgetState extends State<MediaResultWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.file(File(widget.filePath))
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+    _controller.addListener(() {
+      if (!_controller.value.isPlaying &&
+          _controller.value.isInitialized &&
+          (_controller.value.duration == _controller.value.position)) {
+        //checking the duration and position every time
+        setState(() {
+          print(
+              "*************** video paying  c o m p l e t e d *******************");
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('CameraKit Result'),
+        ),
+        floatingActionButton: widget.filePath.isNotEmpty &&
+                widget.fileType == "video"
+            ? FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    _controller.value.isPlaying
+                        ? _controller.pause()
+                        : _controller.play();
+                  });
+                },
+                child: Icon(
+                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                ),
+              )
+            : Container(),
+        body: widget.filePath.isNotEmpty
+            ? widget.fileType == 'video'
+                ? Center(
+                    child: AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    ),
+                  )
+                : widget.fileType == 'image'
+                    ? Image.file(File(widget.filePath))
+                    : const Text("UnKnown File to show")
+            : const Text("No File to show"));
+  }
+}
